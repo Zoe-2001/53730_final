7758e2001e877a617e33a89146dfc17d919dc440
Zoe-2001 <yuchenda@andrew.cmu.edu>
2023-12-05 11:30:35 -0500

    created a textbox system for further usage

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -5,25 +5,37 @@ var rng = RandomNumberGenerator.new()
 @onready var numofsuccess = 0
 
 var entered_green = false
-
 func _on_green_area_area_entered(area):
-	$Label.text = area.name
+	$Label.text = "Click!"
 	entered_green = true
-
 
 func _on_green_area_area_exited(area):
 	$Label.text = ""
 	entered_green = false
 
 
+var entered_blue = false
+func _on_blue_area_area_entered(area):
+	$Label.text = "Click!"
+	entered_blue = true
+
+func _on_blue_area_area_exited(area):
+	$Label.text = ""
+	entered_blue = false
+	
 func _physics_process(delta):
 	var greenarea = %GreenArea
+	var bluearea = %BlueArea
 	%NumofSuccess.text = "Success Count: "+str(numofsuccess)
 	
-	if entered_green == true and Input.is_action_just_pressed("ui_accept"):
-		var min_range = -149
-		var max_range = 208
+	if (entered_blue or entered_green) and Input.is_action_just_pressed("ui_accept"):
+		var min_range = -246
+		var max_range = 117
 		var random_number = rng.randf_range(min_range, max_range)
+		bluearea.position.x = random_number
+		min_range = -70
+		max_range = 288
+		random_number = rng.randf_range(min_range, max_range)
 		greenarea.position.x = random_number
 		numofsuccess += 1
 		
@@ -38,6 +50,10 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+
+
 
 
 
